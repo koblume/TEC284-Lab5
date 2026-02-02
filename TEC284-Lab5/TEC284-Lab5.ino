2f6f8180c8b8ecb23ae34fc796f6b31153345a60
koblume <koblume@ilstu.edu>
2026-02-02 15:55:07 -0600

    Added automation mode capability
    Added autoDriver and distanceTest functions to allow the race car to maneuver around objects.

--- TEC284-Lab5/TEC284-Lab5.ino
+++ TEC284-Lab5/TEC284-Lab5.ino
@@ -6,51 +6,86 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
 
+long duration;
+long distance = 0;
+
 void setup() {
   AFMS.begin();
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  forwards();
-  delay(500);
-  turnLeft();
-  delay(500);
-  turnRight();
-  delay(500);
-  backwards();
-  delay(500);
+  //forwards();
+  //delay(500);
+  //turnLeft();
+  //delay(500);
+  //turnRight();
+  //delay(500);
+  //backwards();
+  //delay(500);
+
+  autoDriver();
 }
 
 void forwards(){
-  rightMotor->setSpeed(100);
-  leftMotor->setSpeed(100);
+  rightMotor->setSpeed(255);
+  leftMotor->setSpeed(255);
 
   rightMotor->run(FORWARD);
   leftMotor->run(FORWARD);
 }
 
 void backwards(){
-  rightMotor->setSpeed(100);
-  leftMotor->setSpeed(100);
-
+  rightMotor->setSpeed(255);
+  leftMotor->setSpeed(255);
+  
   rightMotor->run(BACKWARD);
   leftMotor->run(BACKWARD);
 }
 
 void turnLeft(){
   rightMotor->setSpeed(0);
-  leftMotor->setSpeed(100);
+  leftMotor->setSpeed(255);
 
   rightMotor->run(BACKWARD);
   leftMotor->run(FORWARD);
 }
 
 void turnRight(){
-  rightMotor->setSpeed(100);
+  rightMotor->setSpeed(255);
   leftMotor->setSpeed(0);
 
   rightMotor->run(FORWARD);
   leftMotor->run(BACKWARD);
 }
 
+long distanceTest(){
+  digitalWrite(7,LOW);
+  //delayMicroseconds(2);
+  digitalWrite(7, HIGH);
+  //delayMicroseconds(10);
+  digitalWrite(7, LOW);
+  duration = pulseIn(8, HIGH);
+  distance = duration * 0.0133 /2;
+  Serial.print("Distance = ");
+  Serial.println(distance);
+  return distance;
+  
+}
+
+void autoDriver(){
+  distanceTest();
+  if(distance < 5){
+    backwards();
+    //delay(100);
+    turnLeft();
+    //delay(10);
+  }
+  else{
+    forwards();
+    
+  }
+}
 
